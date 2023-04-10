f656061bad8d5c82e6c392dd5ed3f2691ed76ed6
Xing Wang <xingw@splunk.com>
2023-04-09 18:25:46 -0700

    stage2

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -9,7 +8,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
@@ -17,11 +16,16 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
-	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// output, err := cmd.Output()
+	// err := cmd.Run()
+	cmd.Run()
+	// if err != nil {
+	// 	fmt.Printf("Err: %v", err)
+	// 	os.Exit(1)
+	// }
 
-	fmt.Println(string(output))
+	// fmt.Println(string(output))
 }
